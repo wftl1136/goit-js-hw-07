da3c0ec6b01064e4e274935ea86c7c2a36d7fa49
Maksym <zzzeleznyakmaxim@gmail.com>
2025-03-05 19:51:20 +0200

    full

--- js/task-1.js
+++ js/task-1.js
@@ -1,31 +1,10 @@
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
+const categories = document.querySelectorAll('#categories>li');
+console.log(`Number of categories: ${categories.length}`);
 
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
-
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+categories.forEach(category => {
+    const categoryName = category.firstElementChild.textContent.trim();
+    const categoryList = category.querySelector("ul");
+    const Elements = categoryList ? categoryList.children.length : 0;
+    console.log("Category:", `${categoryName}`);
+    console.log("Elements:", `${Elements}`);
+});
--- js/task-3.js
+++ js/task-3.js
@@ -1,33 +1,6 @@
-class StringBuilder {
-  #value;
+const textInput = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
 
-constructor(value) {
-      this.#value = value;
-  }
-
-  getValue() {
-      return this.#value;
-  }
-
-  padEnd(str) {
-       this.#value += str;
-  }
-  padStart(str) {
-       this.#value = str + this.#value;
-      
-  }
-
-  padBoth(str) {
-this.#value =str + this.#value + str;
-  }
-
-} 
-
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+textInput.addEventListener("input", (event) => {
+  output.textContent = event.currentTarget.value.trim()||"Anonymous";
+});
